a0604016eeaf9b6d1c0f890531b14a74cd381505
Tejas Nevrekar <tejas.nevrekar@gmail.com>
2020-09-11 00:31:03 +0530

    Support micro modules to start with java -jar
    Added pom instructions to allow invocation of
    micro modules as java -jar in stead of the tar.
    
    JIRA: ODLMICRO-31
    Change-Id: I1b24f300c106a4e7e9d4e86343ef70754d150c9c
    Signed-off-by: Tejas Nevrekar <tejas.nevrekar@gmail.com>

---
+++ micro-neutron/src/main/java/org/opendaylight/neutron/micro/NeutronMain.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 Lumina Networks, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.micro;
+
+import org.opendaylight.infrautils.micro.ShellMain;
+import org.opendaylight.odlguice.inject.guice.GuiceClassPathBinder;
+
+public final class NeutronMain {
+
+    private NeutronMain() {
+    }
+
+    public static void main(String[] args) {
+        GuiceClassPathBinder classPathBinder = new GuiceClassPathBinder("org.opendaylight");
+        new ShellMain(new NeutronModule(classPathBinder)).awaitShutdown();
+    }
+}
