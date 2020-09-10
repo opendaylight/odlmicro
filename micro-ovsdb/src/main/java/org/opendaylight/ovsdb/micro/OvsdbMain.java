a0ce46274175f8a2513a36bde15619a84a9d8c4f
Tejas Nevrekar <tejas.nevrekar@gmail.com>
2020-09-11 00:31:03 +0530

    Support micro modules to start with java -jar
    Added pom instructions to allow invocation of
    micro modules as java -jar in stead of the tar.
    
    JIRA: ODLMICRO-31
    Change-Id: I1b24f300c106a4e7e9d4e86343ef70754d150c9c
    Signed-off-by: Tejas Nevrekar <tejas.nevrekar@gmail.com>

---
+++ micro-ovsdb/src/main/java/org/opendaylight/ovsdb/micro/OvsdbMain.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019 Lumina Networks, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.micro;
+
+import org.opendaylight.infrautils.micro.ShellMain;
+import org.opendaylight.odlguice.inject.guice.GuiceClassPathBinder;
+
+public final class OvsdbMain {
+
+    private OvsdbMain() {
+    }
+
+    public static void main(String[] args) {
+        GuiceClassPathBinder classPathBinder = new GuiceClassPathBinder("org.opendaylight");
+        new ShellMain(new OvsdbModule(classPathBinder)).awaitShutdown();
+    }
+}
