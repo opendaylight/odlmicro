3e390f9dbedc7ab02625e1e1ae5f4b71f3230fc5
VenkataSatya Jonnadula <ravi.jonnadula@gmail.com>
2020-11-28 23:35:02 +0530

    odlmicro - RPC's are not supported.
    This issue was due to schemacontext was null in DOMRpcRouter.
    Fixed issue by setting the schemacontext.
    
    JIRA-ID: ODLMICRO-43
    
    Signed-off-by: VenkataSatya Jonnadula <ravi.jonnadula@gmail.com>
    Change-Id: I844adfe274d0036e30d920c4aab3dfe7818093e3

--- micro-core/src/main/java/org/opendaylight/mdsal/micro/binding/dom/adapter/MockSchemaService.java
+++ micro-core/src/main/java/org/opendaylight/mdsal/micro/binding/dom/adapter/MockSchemaService.java
@@ -22,6 +22,7 @@ import org.opendaylight.mdsal.binding.dom.codec.spi.BindingDOMCodecServices;
 import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeContext;
 import org.opendaylight.mdsal.dom.api.DOMSchemaService;
 import org.opendaylight.mdsal.dom.api.DOMSchemaServiceExtension;
+import org.opendaylight.mdsal.dom.broker.DOMRpcRouter;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.util.ListenerRegistry;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
@@ -54,7 +55,11 @@ public final class MockSchemaService implements DOMSchemaService, EffectiveModel
     @Override
     public ListenerRegistration<EffectiveModelContextListener> registerSchemaContextListener(
             final EffectiveModelContextListener listener) {
-        return listeners.register(listener);
+        ListenerRegistration<EffectiveModelContextListener> reg =  listeners.register(listener);
+        if (listener instanceof DOMRpcRouter) {
+            listener.onModelContextUpdated(schemaContext);
+        }
+        return reg;
     }
 
     @Override
